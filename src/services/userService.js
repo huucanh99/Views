7d63758b2e1d8920144d30fe2b52fe56796f69d4
huucanh99 <huucanh28092015@gmail.com>
2024-10-31 13:42:52 +0700

    get AllUsers

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,4 +3,7 @@ import axios from '../axios';
 const handleLoginApi = (email, password) => {
     return axios.post('/api/login', { email, password });
 }
-export { handleLoginApi }
+const getAllUser = (userId) => {
+    return axios.get(`/api/get-all-users?id=${userId}`);
+}
+export { handleLoginApi, getAllUser }
