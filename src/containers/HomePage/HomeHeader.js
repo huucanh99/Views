034df10eb4332a34d78270707ebb1632ce239f74
huucanh99 <huucanh28092015@gmail.com>
2024-11-14 11:05:11 +0700

    use redux change language

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,9 +3,15 @@ import { connect } from 'react-redux';
 import './HomeHeader.scss';
 import logo from '../../assets/logo.jpg';
 import { FormattedMessage } from 'react-intl'
+import { LANGUAGES } from '../../utils';
+import { changeLanguageApp } from '../../store/actions';
 class HomeHeader extends Component {
-
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language)
+    }
     render() {
+        let language = this.props.language;
+        console.log('check language', language)
         return (
             <React.Fragment>
                 <div className="home-header-container" >
@@ -34,8 +40,8 @@ class HomeHeader extends Component {
                         </div>
                         <div className='right-content'>
                             <div className='support'><i className='fas fa-question-circle'></i><FormattedMessage id="homeheader.support" /></div>
-                            <div className='language-vi'>VN</div>
-                            <div className='language-en'>EN</div>
+                            <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}><span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
+                            <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}><span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
                         </div>
                     </div>
                 </div>
@@ -55,23 +61,23 @@ class HomeHeader extends Component {
                                 <div className='text-child'><FormattedMessage id="banner.child1" /></div>
                             </div>
                             <div className='option-child'>
-                                <div className='icon-child'><i className='far fa-mobile-alt'></i></div>
+                                <div className='icon-child'><i className='far fa-hospital'></i></div>
                                 <div className='text-child'><FormattedMessage id="banner.child2" /></div>
                             </div>
                             <div className='option-child'>
-                                <div className='icon-child'><i className='far fa-procedures'></i></div>
+                                <div className='icon-child'><i className='far fa-hospital'></i></div>
                                 <div className='text-child'><FormattedMessage id="banner.child3" /></div>
                             </div>
                             <div className='option-child'>
-                                <div className='icon-child'><i className='far fa-flask'></i></div>
+                                <div className='icon-child'><i className='far fa-hospital-m'></i></div>
                                 <div className='text-child'><FormattedMessage id="banner.child4" /></div>
                             </div>
                             <div className='option-child'>
-                                <div className='icon-child'><i className='far fa-user-md'></i></div>
+                                <div className='icon-child'><i className='far fa-hospital'></i></div>
                                 <div className='text-child'><FormattedMessage id="banner.child5" /></div>
                             </div>
                             <div className='option-child'>
-                                <div className='icon-child'><i className='far fa-briefcase-medical'></i></div>
+                                <div className='icon-child'><i className='far fa-hospital'></i></div>
                                 <div className='text-child'><FormattedMessage id="banner.child6" /></div>
                             </div>
                         </div>
@@ -85,11 +91,14 @@ class HomeHeader extends Component {
 
 const mapStateToProps = state => {
     return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
     };
 };
 
--- src/store/reducers/appReducer.js
+++ src/store/reducers/appReducer.js
@@ -18,12 +18,17 @@ const initialState = {
 
 const appReducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.APP_START_UP_COMPLETE: 
+        case actionTypes.APP_START_UP_COMPLETE:
             return {
                 ...state,
                 started: true
             }
-        case actionTypes.SET_CONTENT_OF_CONFIRM_MODAL: 
+        case actionTypes.CHANGE_LANGUAGE:
+            return {
+                ...state,
+                language: action.language
+            }
+        case actionTypes.SET_CONTENT_OF_CONFIRM_MODAL:
             return {
                 ...state,
                 contentOfConfirmModal: {
